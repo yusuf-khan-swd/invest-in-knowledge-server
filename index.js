1034f8833a2deceb491006a649d2213d8b967ed3
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-25 17:50:44 +0600

    added category api and vercel to project

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+const category = require('./data/data.json');
+
 app.get('/', (req, res) => {
   res.send('Server is running.');
+});
+
+app.get('/category', (req, res) => {
+  res.send(category);
 });
 
 app.listen(port, () => {
